6c6fb3e99929afc21a092c4f3ee8639bdb2a1875
GGVidal <gabrielvidalgondim@gmail.com>
2020-12-05 15:34:17 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, {useState} from 'react';
+import axios from 'axios';
+const App = () => {
+  const [minDate, setMinDate] = useState(0);
+  const [maxDate, setMaxDate] = useState(0);
+  const onRequest = async() => {
+    axios({
+      url: `http://localhost:5000/pdf/seguro.xls?min_date=${minDate}&max_date=${maxDate}`, //your url
+      method: 'GET',
+      responseType: 'blob', // important
+    }).then((response) => {
+       const url = window.URL.createObjectURL(new Blob([response.data]));
+       const link = document.createElement('a');
+       link.href = url;
+       link.setAttribute('download', 'file.xls'); //or any other extension
+       document.body.appendChild(link);
+       link.click();
+    }).catch(error => {console.log(error)});
+  }
+  return(
+    <div>
+      <input onChange={(e)=> setMinDate(e.target.value)}/>
+      <input onChange={(e)=> setMaxDate(e.target.value)}/>
+      <button onClick={() => onRequest()}>Click</button>
     </div>
-  );
+  )
 }
-
 export default App;

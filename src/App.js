c08ccc21be5c8aef8a26e5f4df94dba9c58d84e9
GGVidal <gabrielvidalgondim@gmail.com>
2020-12-08 21:53:52 -0300

    CHG: making post request in download

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import { saveAs } from "file-saver";
+import axios from "axios";
 const App = () => {
   const [minDate, setMinDate] = useState(0);
   const [maxDate, setMaxDate] = useState(0);
-  const onRequest = async() => {
+  const onRequest = async () => {
     axios({
-      url: `http://localhost:5000/pdf/seguro.xls?min_date=${minDate}&max_date=${maxDate}`, //your url
-      method: 'GET',
-      responseType: 'blob', // important
+      url: "http://127.0.0.1:5000/pdf/seguro.xls",
+      method: "POST",
+      responseType: "blob",
+      data: { min_date: minDate, max_date: maxDate }, // important
     }).then((response) => {
-       const url = window.URL.createObjectURL(new Blob([response.data]));
-       const link = document.createElement('a');
-       link.href = url;
-       link.setAttribute('download', 'file.xls'); //or any other extension
-       document.body.appendChild(link);
-       link.click();
-    }).catch(error => {console.log(error)});
-  }
-  return(
+      saveAs(response.data, "file.xls");
+    }); 
+  };
+  return (
     <div>
-      <input onChange={(e)=> setMinDate(e.target.value)}/>
-      <input onChange={(e)=> setMaxDate(e.target.value)}/>
+      <input onChange={(e) => setMinDate(e.target.value)} />
+      <input onChange={(e) => setMaxDate(e.target.value)} />
       <button onClick={() => onRequest()}>Click</button>
     </div>
-  )
-}
+  );
+};
 export default App;

662007e8c4634f1b88bd519e94d501a6b1a4f582
GGVidal <gabrielvidalgondim@gmail.com>
2021-01-05 22:29:25 -0300

    Styling main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 import { saveAs } from "file-saver";
 import axios from "axios";
-import {DatePicker} from 'antd'
+import { DatePicker, Layout, Button, Row, Col } from "antd";
+const { Header, Content } = Layout;
+
 const App = () => {
   const [minDate, setMinDate] = useState(0);
   const [maxDate, setMaxDate] = useState(0);
@@ -14,17 +16,50 @@ const App = () => {
       responseType: "blob",
       data: { min_date: minDate, max_date: maxDate, start_date: startDate }, // important
     }).then((response) => {
-      saveAs(response.data, "file.xls");
+      saveAs(response.data, "seguro.xls");
     });
   };
-  console.log(minDate);
+
   return (
-    <div>
-      <DatePicker onChange={(date, dateString) => {setMinDate(dateString)}} format="DD/MM/YYYY"/>
-      <DatePicker onChange={(date, dateString) => {setMaxDate(dateString)}} format="DD/MM/YYYY"/>
-      <DatePicker onChange={(date, dateString) => {setStartDate(dateString)}} format="DD/MM/YYYY"/>
-      <button onClick={() => onRequest()}>Click</button>
-    </div>
+    <Layout>
+      <Header style={{ position: "fixed", zIndex: 1, width: "100%", backgroundColor: '#388E3C' }}>
+        <div className="logo" />
+      </Header>
+      <Content
+        className="site-layout"
+        style={{ padding: "0 50px", marginTop: 84 }}
+      >
+        <div className="site-layout-background" style={{ minHeight: "42em" }}>
+          <Row align="center" gutter={12}>
+            <Col>
+              <DatePicker
+                onChange={(date, dateString) => {
+                  setMinDate(dateString);
+                }}
+                format="DD/MM/YYYY"
+              />
+            </Col>
+            <Col>
+              <DatePicker
+                onChange={(date, dateString) => {
+                  setMaxDate(dateString);
+                }}
+                format="DD/MM/YYYY"
+              />
+            </Col>
+            <Col>
+              <DatePicker
+                onChange={(date, dateString) => {
+                  setStartDate(dateString);
+                }}
+                format="DD/MM/YYYY"
+              />
+            </Col>
+            <Button style={{backgroundColor: '#8BC34A'}} shape="round" onClick={() => onRequest()}>Gerar</Button>
+          </Row>
+        </div>
+      </Content>
+    </Layout>
   );
 };
 export default App;

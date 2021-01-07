c579bbe275c38e0bce5151ccafab59bafde6594d
GGVidal <gabrielvidalgondim@gmail.com>
2021-01-06 22:07:14 -0300

    NEW: Creating layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { useState } from "react";
 import "antd/dist/antd.css";
 import { saveAs } from "file-saver";
 import axios from "axios";
-import { DatePicker, Layout, Button, Row, Col } from "antd";
+import { DatePicker, Layout, Button, Form, Typography, Row } from "antd";
+
 const { Header, Content } = Layout;
+const { Title } = Typography;
 
 const App = () => {
+  const layout = {
+    labelCol: { span: 11 },
+    wrapperCol: { span: 16 },
+  };
   const [minDate, setMinDate] = useState(0);
   const [maxDate, setMaxDate] = useState(0);
   const [startDate, setStartDate] = useState(0);
+
+  const tailLayout = {
+    wrapperCol: { offset: 11, span: 16 },
+  };
+
+  const onFinish = (values) => {
+    onRequest().then()
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   const onRequest = async () => {
     axios({
       url: "http://127.0.0.1:5000/pdf/seguro.xls",
@@ -22,41 +40,98 @@ const App = () => {
 
   return (
     <Layout>
-      <Header style={{ position: "fixed", zIndex: 1, width: "100%", backgroundColor: '#388E3C' }}>
+      <Header
+        style={{
+          zIndex: 1,
+          width: "100%",
+          backgroundColor: "#388E3C",
+        }}
+      >
         <div className="logo" />
       </Header>
       <Content
         className="site-layout"
-        style={{ padding: "0 50px", marginTop: 84 }}
+        style={{ padding: "0 50px", marginTop: "2em" }}
       >
         <div className="site-layout-background" style={{ minHeight: "42em" }}>
-          <Row align="center" gutter={12}>
-            <Col>
+          <Row align="center">
+            <Title>Seguro Ítaca</Title>
+          </Row>
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label="Data de Inicio"
+              name="dataInicio"
+              style={{ marginTop: "2em" }}
+              rules={[
+                {
+                  required: true,
+                  message: "Por favor preencha a data de inicio",
+                },
+              ]}
+            >
               <DatePicker
+                placeholder=""
+                format="DD/MM/YYYY"
                 onChange={(date, dateString) => {
                   setMinDate(dateString);
                 }}
-                format="DD/MM/YYYY"
               />
-            </Col>
-            <Col>
+            </Form.Item>
+
+            <Form.Item
+              label="Data de fim"
+              name="dataFim"
+              rules={[
+                {
+                  required: true,
+                  message: "Por favor preencha a data de fim",
+                },
+              ]}
+            >
               <DatePicker
+                placeholder=""
+                format="DD/MM/YYYY"
                 onChange={(date, dateString) => {
                   setMaxDate(dateString);
                 }}
-                format="DD/MM/YYYY"
               />
-            </Col>
-            <Col>
+            </Form.Item>
+            <Form.Item
+              label="Data de inicio preenchimento"
+              name="dataPreenchimento"
+              extra="Colocar a data de inicio do preenchimento do formulário"
+              rules={[
+                {
+                  required: true,
+                  message:
+                    "Por favor preencha a data de inicio de preenchimento",
+                },
+              ]}
+            >
               <DatePicker
+                placeholder=""
+                format="DD/MM/YYYY"
                 onChange={(date, dateString) => {
                   setStartDate(dateString);
                 }}
-                format="DD/MM/YYYY"
               />
-            </Col>
-            <Button style={{backgroundColor: '#8BC34A'}} shape="round" onClick={() => onRequest()}>Gerar</Button>
-          </Row>
+            </Form.Item>
+            <Form.Item {...tailLayout}>
+              <Button
+                style={{ backgroundColor: "#8BC34A" }}
+                shape="round"
+                htmlType="submit"
+              >
+                Gerar
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
       </Content>
     </Layout>

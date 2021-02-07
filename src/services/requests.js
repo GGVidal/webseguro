d2a1b8d6b97fd3df3ae97efe0432a3d723169f6c
GGVidal <gabrielvidalgondim@gmail.com>
2021-02-07 14:41:19 -0300

    Testing

--- src/services/requests.js
+++ src/services/requests.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-const { REACT_APP_PROD_URL } = process.env
+const { REACT_APP_PROD_URL } = process.env;
 const BASE_URL_PRODUCTION = `${REACT_APP_PROD_URL}`;
 // const BASE_URL_DEV = "http://127.0.0.1:8080";
 
 const onLogin = ({ name, password }) => {
+  console.log("URL", `${BASE_URL_PRODUCTION}/login`);
   let bodyFormData = new FormData();
   bodyFormData.append("name", name);
   bodyFormData.append("password", password);
@@ -19,15 +20,15 @@ const onLogin = ({ name, password }) => {
 };
 
 const onDownload = (token, obj) => {
-    return axios({
-      url: `${BASE_URL_PRODUCTION}/pdf/seguro.xls`,
-      method: "POST",
-      responseType: "blob",
-      data: obj,
-      headers: {
-        "x-access-token": token
-      }
-    })
-  };
+  return axios({
+    url: `${BASE_URL_PRODUCTION}/pdf/seguro.xls`,
+    method: "POST",
+    responseType: "blob",
+    data: obj,
+    headers: {
+      "x-access-token": token,
+    },
+  });
+};
 
-  export {onLogin, onDownload}
+export { onLogin, onDownload };
